8eb9cd0603d6c8fbbbee22241d4675070f50a260
tessajohnson005 <tessajohnson005@gmail.com>
2026-09-10 23:50:35 -0500

    WIP

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,7 @@ public class PlayerMovement : MonoBehaviour
     public bool canInteract = false;
     public GameObject item;
     public GameObject screen;
+    public GameObject levelTransition;
 
     //-----Animation
     private Animator animator;
@@ -28,6 +29,15 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         rb.linearVelocity = moveInput * moveSpeed;
+
+        if (!item.activeSelf) //if item is picked up
+        {
+            levelTransition.SetActive(true);
+        }
+        else
+        {
+            levelTransition.SetActive(false);
+        }
 
         if (moveInput != Vector2.zero)
         {
